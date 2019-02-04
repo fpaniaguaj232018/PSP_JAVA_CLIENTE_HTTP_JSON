d74fe00b9e886c6c98f442c5964e20a8316e7406
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2019-02-04 17:06:17 +0100

    Elimina librerías no utilizadas

--- src/com/fernandopaniagua/client/Ejecutador.java
+++ src/com/fernandopaniagua/client/Ejecutador.java
@@ -6,8 +6,6 @@
 package com.fernandopaniagua.client;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
